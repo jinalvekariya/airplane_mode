0b44bfb10605ac719f1a10dc150b8a92f9eb605d
romit-sigzen <romit.d@sigzen.com>
2023-09-21 18:10:55 +0530

    Doctype changes

--- airplane_mode/airplane_mode/doctype/airplane_ticket_add_on_item/airplane_ticket_add_on_item.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket_add_on_item/airplane_ticket_add_on_item.py
@@ -1,9 +1,8 @@
 # Copyright (c) 2023, Jinal Balar and contributors
 # For license information, please see license.txt
 
-import frappe
+# import frappe
 from frappe.model.document import Document
 
-class FlightPassenger(Document):
-	def before_save(self):
-     		self.full_name = self.first_name + " " + (self.last_name or '')
+class AirplaneTicketAddonItem(Document):
+	pass
