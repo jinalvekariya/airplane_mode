b90ebaa528fa9ea1fd4456d624e48c28c8ee9744
romit-sigzen <romit.d@sigzen.com>
2023-09-22 18:38:58 +0530

    New Changes

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2023, Jinal Balar and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.model.document import Document
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
 
-class AirplaneFlight(Document):
-	pass
+class AirplaneFlight(WebsiteGenerator):
+    def on_submit(self):
+     	if self.status == "Scheduled":
+          	self.status = "Completed"
+		
