18bcecdcfd5f60b8608310e3eb72153ad433081d
romit-sigzen <romit.d@sigzen.com>
2023-09-25 17:22:42 +0530

    notification

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Airline', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        // Add a custom button to open the airline's website
+        if (frm.doc.website) {
+            frm.add_custom_button(__('Open Website'), function() {
+				console.log(frm.doc.website)
+                window.open(frm.doc.website, '_blank');
+            }).addClass('btn-primary');
+        }
+    }
 });
